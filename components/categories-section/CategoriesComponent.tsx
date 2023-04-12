d58f843d5ebfe5f02913c0200e467e796f9ef240
kellytbn <kellytbn14@gmail.com>
2023-04-11 22:36:54 -0500

    Se agrega seccion categorias

---
+++ components/categories-section/CategoriesComponent.tsx
@@ -0,0 +1,80 @@
+import {Container} from "@mui/material";
+import SectionHeaderComponent from "@/components/SectionHeaderComponent";
+
+const CategoriesComponent = () => {
+    const posts = [
+        {
+            img: "/media/main/categorias-populares/categorias-item1.png",
+            info: "Carros, Motos y Otros"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item2.png",
+            info: "Computación"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item3.png",
+            info: "Ropa y Accesorios"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item4.png",
+            info: "Hogar y Muebles"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item5.png",
+            info: "Accesorios para Vehículos"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item6.png",
+            info: "Electrodomésticos"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item7.png",
+            info: "Cámaras y Accesorios"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item8.png",
+            info: "Celulares y Teléfonos"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item9.png",
+            info: "Belleza y Cuidado Personal"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item10.png",
+            info: "Deportes y Fitness"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item11.png",
+            info: "Electrónica, Audio y Video"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item12.png",
+            info: "Consolas y Videojuegos"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item13.png",
+            info: "Juegos y Juguetes"
+        },
+        {
+            img: "/media/main/categorias-populares/categorias-item14.png",
+            info: "Herramientas"
+        },
+    ];
+    return (
+        <Container>
+            <div className="mt-20">
+                <SectionHeaderComponent title={"Categorías populares"} subtitle={""} isSubtitleList={false}></SectionHeaderComponent>
+                <div className="grid grid-cols-7 gap-0.5 mb-10 bg-opacity-0.002 shadow-sm rounded-4">
+                    {posts.map((item, key) => (
+                        <div className="box-border bg-white border-b border-r border-gray-300 flex flex-col justify-center items-center pt-10 pb-10" key={key}>
+                            <img src={item.img} alt=""/>
+                            <span className="font-normal text-xs text-gray-700 ">{item.info}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+export default CategoriesComponent;
