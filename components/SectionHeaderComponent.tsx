7b2aebdd5cdc15b884e30047719efe4cf525c90a
kellytbn <kellytbn14@gmail.com>
2023-04-13 00:10:38 -0500

    Se agrega responsive a las secciones colecciones, categorias y footer

--- components/SectionHeaderComponent.tsx
+++ components/SectionHeaderComponent.tsx
@@ -8,14 +8,14 @@ interface SectionHeaderComponentProps {
 
 const SectionHeaderComponent = ({title, subtitle, isSubtitleList}: SectionHeaderComponentProps) => {
     return (
-        <div className="flex flex-row">
-            <h2>
-                <span className="text-2xl font-light text-gray-400">{title} </span>
+        <div className="flex flex-col sm:flex-row">
+            <h2 className="text-2xl font-light text-gray-400 sm:mr-4">
+                <span className="text-base sm:text-2xl font-light text-gray-400">{title} </span>
                 {!isSubtitleList && (
-                    <a className="text-base mb-1 text-blue-500" href="#">{subtitle}</a>
+                    <a className="text-xs sm:text-base ml-2 sm:ml-0 text-blue-500" href="#">{subtitle}</a>
                 )}
                 {isSubtitleList && (
-                    <a className="text-2xl font-semibold text-gray-500" href="#">{subtitle}
+                    <a className="text-xs sm:text-2xl font-semibold text-gray-500 ml-2 sm:ml-0" href="#">{subtitle}
                         <button className='icon-dark text-blue-dropdown'>
                             <MdKeyboardArrowDown/>
                         </button>
