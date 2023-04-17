194e8d73671b638c54be4a8a2488db8bafd0e60d
kellytbn <kellytbn14@gmail.com>
2023-04-16 23:28:06 -0500

    config eslintrc

--- components/categories_section/CategoriesItemComponent.tsx
+++ components/categories_section/CategoriesItemComponent.tsx
@@ -1,22 +1,31 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 interface CategoriesItemComponentProps {
-    idKey: any;
-    src: string;
-    title: string;
-    total:number
+  idKey: number;
+  src: string;
+  title: string;
+  total: number;
 }
 
-const CategoriesItemComponent = ({idKey, src, title,total}: CategoriesItemComponentProps) => {
-    return (
-        
-        <div
-            className={`box-border bg-white border-b border-r ${(total/2)-1> Number(idKey)-1 ? ""  :  "lg:border-b-0" } ${(total/2)-1== Number(idKey) || (total)-1== Number(idKey) ? "lg:border-r-0"  :  "" }  border-gray-300 flex flex-col justify-center items-center pt-10 pb-10`}
-            key={idKey}>
-            <Image src={src} alt={""} width={49} height={49} />
-            <span className="font-normal text-xs text-gray-700 ">{title}</span>
-        </div>
-    )
-}
+const CategoriesItemComponent = ({
+  idKey,
+  src,
+  title,
+  total,
+}: CategoriesItemComponentProps) => (
+  <div
+    className={`box-border border-b border-r bg-white ${
+      total / 2 - 1 > Number(idKey) - 1 ? '' : 'lg:border-b-0'
+    } ${
+      total / 2 - 1 == Number(idKey) || total - 1 == Number(idKey)
+        ? 'lg:border-r-0'
+        : ''
+    }  flex flex-col items-center justify-center border-gray-300 pb-10 pt-10`}
+    key={idKey}
+  >
+    <Image src={src} alt={''} width={49} height={49} />
+    <span className='text-xs font-normal text-gray-700 '>{title}</span>
+  </div>
+);
 
 export default CategoriesItemComponent;
--- components/footer_section/FooterComponent.tsx
+++ components/footer_section/FooterComponent.tsx
@@ -1,47 +1,59 @@
 import React from 'react';
-import {FooterInfoComponent} from "@/components/footer_section/FooterInfoComponent";
-import Image from "next/image";
+import { FooterInfoComponent } from '@/components/footer_section/FooterInfoComponent';
+import Image from 'next/image';
 
-const FooterComponent = () => {
-    return (
-        <footer className="m-auto max-w-[1200px] w-full py-[40px]">
-            <FooterInfoComponent></FooterInfoComponent>
-            <div className="bg-white border-t border-gray-300">
-                <div className="mt-4">
-                    <div>
-                        <ul className="flex flex-wrap justify-center md:justify-start md:ml-0">
-                            <li className="font-normal text-xs leading-4 flex items-start mb-2 mr-4 text-gray-700">Trabaja
-                                con
-                                nosotros
-                            </li>
-                            <li className="font-normal text-xs leading-4 flex items-start mb-2 mr-4 text-gray-700">Términos
-                                y condiciones
-                            </li>
-                            <li className="font-normal text-xs leading-4 flex items-start mb-2 mr-4 text-gray-700">Cómo
-                                cuidamos tu privacidad
-                            </li>
-                            <li className="font-normal text-xs leading-4 flex items-start mb-2 mr-4 text-gray-700">Accesibilidad</li>
-                            <li className="font-normal text-xs leading-4 flex items-start mb-2 mr-4 text-gray-700">Ayuda
-                                / PQR
-                            </li>
-                            <li className="font-normal text-xs leading-4 flex items-start mb-2 mr-4 text-gray-700">www.sic.gov.co</li>
-                        </ul>
-                    </div>
-                    <div className="flex flex-col gap-2 mt-1 items-center md:items-start">
-                        <span
-                            className="font-normal text-xs leading-4 text-gray-9">Copyright © 1999-2023 MercadoLibre Colombia LTDA.</span>
-                        <span className="font-normal text-xs leading-4 text-gray-9">Carrera 17 Numero 93 - 09 Piso 3, Bogotá D.C., Colombia</span>
-                    </div>
-                </div>
-                <div
-                    className="flex flex-col gap-5 pt-5 sm:px-1 border-t border-gray-300 mt-5 items-center md:flex-row md:items-start">
-                    <Image src="/media/main/footer/footer-logo-industria-comercio.png" alt={""} width={141}
-                           height={30}/>
-                    <Image src="/media/main/footer/footer-logo-pare.png" alt={""} width={200} height={34}/>
-                </div>
-            </div>
-        </footer>
-    );
-}
+const FooterComponent = () => (
+  <footer className='m-auto w-full max-w-[1200px] py-[40px]'>
+    <FooterInfoComponent></FooterInfoComponent>
+    <div className='border-t border-gray-300 bg-white'>
+      <div className='mt-4'>
+        <div>
+          <ul className='flex flex-wrap justify-center md:ml-0 md:justify-start'>
+            <li className='mb-2 mr-4 flex items-start text-xs font-normal leading-4 text-gray-700'>
+              Trabaja con nosotros
+            </li>
+            <li className='mb-2 mr-4 flex items-start text-xs font-normal leading-4 text-gray-700'>
+              Términos y condiciones
+            </li>
+            <li className='mb-2 mr-4 flex items-start text-xs font-normal leading-4 text-gray-700'>
+              Cómo cuidamos tu privacidad
+            </li>
+            <li className='mb-2 mr-4 flex items-start text-xs font-normal leading-4 text-gray-700'>
+              Accesibilidad
+            </li>
+            <li className='mb-2 mr-4 flex items-start text-xs font-normal leading-4 text-gray-700'>
+              Ayuda / PQR
+            </li>
+            <li className='mb-2 mr-4 flex items-start text-xs font-normal leading-4 text-gray-700'>
+              www.sic.gov.co
+            </li>
+          </ul>
+        </div>
+        <div className='mt-1 flex flex-col items-center gap-2 md:items-start'>
+          <span className='text-xs font-normal leading-4 text-gray-9'>
+            Copyright © 1999-2023 MercadoLibre Colombia LTDA.
+          </span>
+          <span className='text-xs font-normal leading-4 text-gray-9'>
+            Carrera 17 Numero 93 - 09 Piso 3, Bogotá D.C., Colombia
+          </span>
+        </div>
+      </div>
+      <div className='mt-5 flex flex-col items-center gap-5 border-t border-gray-300 pt-5 sm:px-1 md:flex-row md:items-start'>
+        <Image
+          src='/media/main/footer/footer-logo-industria-comercio.png'
+          alt={''}
+          width={141}
+          height={30}
+        />
+        <Image
+          src='/media/main/footer/footer-logo-pare.png'
+          alt={''}
+          width={200}
+          height={34}
+        />
+      </div>
+    </div>
+  </footer>
+);
 
-export {FooterComponent}
+export { FooterComponent };
