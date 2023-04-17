378a0821928366477eedd4ea97723c4b7d8b4697
Jhon Ballesteros <Jhvarga@gap.com>
2023-04-17 00:04:17 -0500

    eslint

--- components/banner.tsx
+++ components/banner.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import Image from 'next/image';
 const BannerComponent = () => (
   <div className='hidden w-full sm:flex'>
-    <Image className='w-full' alt='' src='media/banner.jpg'></Image>
+    <Image
+      className='w-full'
+      alt=''
+      width={1000}
+      height={500}
+      src='/media/banner.jpg'
+    ></Image>
   </div>
 );
 
