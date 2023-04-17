67665ba2fd2c6d9b0b861df9be76df77f2e9b5e9
kellytbn <kellytbn14@gmail.com>
2023-04-16 23:03:53 -0500

    config eslintrc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"/>
-      </Head>
-      <body >
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+const Document = () => (
+  <Html lang='en'>
+    <Head>
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
